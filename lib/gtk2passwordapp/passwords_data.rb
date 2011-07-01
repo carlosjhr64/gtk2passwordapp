d99f4f1c1ec643bcaf22faf7da86a8ae39633cf2
CarlosJHR64 <carlosjhr64@gmail.com>
2011-07-01 15:50:46 -0700

    added test/passwords_data.rb

--- lib/gtk2passwordapp/passwords_data.rb
+++ lib/gtk2passwordapp/passwords_data.rb
@@ -4,8 +4,6 @@ require 'digest/md5'
 module Gtk2Password
 # PasswordsData maitains passwords
 class PasswordsData
-  attr_accessor :account
-  attr_reader :data, :dumpfile
 
   PASSWORD	= 0
   PREVIOUS	= 1
@@ -14,17 +12,19 @@ class PasswordsData
   URL		= 4
   LAST_UPDATE	= 5
 
-  attr_accessor :expired
   def reset(password)
     raise "password too short" if password.length < 7
-    @passphrase = Digest::MD5.digest( password )
-    raise "bad passphrase length" unless @passphrase.length == IOCrypt::LENGTH
-    @dumpfile = File.join( PASSWORDS_DATA_DIR, 'gtk2passwordapp2.dat' )
-    @expired = 60*60*24*30*3 # 3 months
+    # MD5 digest length is 16 to match tiny encryption's block length
+    passphrase = Digest::MD5.digest( password )
+    @iocrypt =  IOCrypt.new(passphrase)
   end
 
-  def initialize(passphrase)
-    reset(passphrase)
+  attr_reader :data
+  attr_accessor :expired, :dumpfile
+  def initialize(password,dumpfile)
+    reset(password) # sets @iocrypt
+    @dumpfile = dumpfile
+    @expired = 60*60*24*30*3 # 3 months
     @data = {}
   end
 
@@ -32,24 +32,29 @@ class PasswordsData
     File.exist?(@dumpfile)
   end
 
-  # will raise an exception on failed decryption TODO verify this
+  # will raise an exception on failed decryption
   def load
-    iocrypt = IOCrypt.new(@passphrase)
-    @data = iocrypt.load(@dumpfile)
+    data = @iocrypt.load(@dumpfile)
+    raise "decryption error" unless data.class == Hash
+    @data = data
   end
 
-  def save(password = nil)
+  def save
     # just in case, keep a backup
     File.rename(@dumpfile, @dumpfile+'.bak') if File.exist?(@dumpfile)
-    iocrypt = IOCrypt.new(@passphrase)
-    iocrypt.dump(@dumpfile, @data)
+    @iocrypt.dump(@dumpfile, @data)
     File.chmod(0600, @dumpfile)
+  end
+
+  def save!(password)
+    reset(password)
+    save
   end
 
   def add(account)
     raise "pre-existing" unless @data[account].nil?
     raise "can't have nil account" if account.nil?
-    @data[account] = ['','','','','']
+    @data[account] = [ '', '', '', '', '', 0 ]
   end
 
   def accounts
@@ -77,7 +82,7 @@ class PasswordsData
 
   def self.url_of(data_account, url)
     data_account[URL] = url unless url.nil?
-    return data_account[URL]	|| ''
+    return data_account[URL]
   end
 
   def url_of(account, url=nil)
@@ -87,8 +92,7 @@ class PasswordsData
   def expired?(account)
     data_account = get_data_account(account)
     last_update = data_account[LAST_UPDATE]
-    return true if last_update.nil? || ((Time.now.to_i - last_update) > @expired)
-    return false
+    ((Time.now.to_i - last_update) > @expired)
   end
 
   def self.password_of(data_account,password)
@@ -100,18 +104,18 @@ class PasswordsData
   def password_of(account, password=nil)
     data_account = get_data_account(account)
     PasswordsData.password_of(data_account,password) unless password.nil?
-    return data_account[PASSWORD]	|| ''
+    return data_account[PASSWORD]
   end
 
   # previous password
   def previous_password_of(account)
     data_account = get_data_account(account)
-    return data_account[PREVIOUS]	|| ''
+    return data_account[PREVIOUS]
   end
 
   def self.note_of(data_account,note)
     data_account[NOTE] = note unless note.nil?
-    return data_account[NOTE]	|| ''
+    return data_account[NOTE]
   end
 
   def note_of(account, note=nil)
@@ -120,7 +124,7 @@ class PasswordsData
 
   def self.username_of(data_account,usr)
     data_account[USERNAME] = usr unless usr.nil?
-    return data_account[USERNAME]	|| ''
+    return data_account[USERNAME]
   end
 
   def username_of(account, usr=nil)
